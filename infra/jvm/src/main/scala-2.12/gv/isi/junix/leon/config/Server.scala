140ce4db9d2167a1a0031272961b344415ad6aa6
Nikos Mouchtaris <mouchtaris@nike.com>
2019-02-13 12:52:33 +0000

    a fix?

--- infra/jvm/src/main/scala-2.12/gv/isi/junix/leon/config/Server.scala
+++ infra/jvm/src/main/scala-2.12/gv/isi/junix/leon/config/Server.scala
@@ -9,10 +9,11 @@ object Server {
   val HOST: String = "host"
   val PORT: String = "port"
 
-  final implicit class Ext(val value: tsConfig)
+  final implicit class Ext(val root: tsConfig)
     extends AnyVal
     with ConfigExt
   {
+    def value: tsConfig = root
     def host: String = value getString HOST
     def port: Int = value getInt PORT
   }
